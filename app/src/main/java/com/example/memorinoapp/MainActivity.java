91608ed6892856610c57575451c5d9c9c743c31d
PilarVaquero <maria.del.pilar.vaquero@gmail.com>
2023-04-24 13:01:54 +0200

    Creación pantalla inicial

--- app/src/main/java/com/example/memorinoapp/MainActivity.java
+++ app/src/main/java/com/example/memorinoapp/MainActivity.java
@@ -2,28 +2,32 @@ package com.example.memorinoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton btn_kiwi;
-
-    int imagenes;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    //método para mostrar y ocultar el menú overflow, método booleano solo va a ser true o false
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.overflow, menu);
+        return true;
+    }
 
-        btn_kiwi = (ImageButton) findViewById(R.id.btn_kiwi);
-
-
+    //método para el botón jugar
+    public void Play(View view){
+        Intent play = new Intent(this, Nivel1Activity.class );
+        startActivity(play);
     }
 
 }
---
+++ app/src/main/java/com/example/memorinoapp/Nivel1Activity.java
@@ -0,0 +1,21 @@
+package com.example.memorinoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Nivel1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nivel1);
+    }
+    //Método para el botón volver
+    public void Volver(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+}
